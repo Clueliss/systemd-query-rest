cf9135459cd81750c034b6a4d266d74a473582cb
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2021-01-04 01:20:57 +0100

    using sh to bind stdout and stderr

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![feature(decl_macro, array_methods, proc_macro_hygiene)]
+#![feature(cstring_from_vec_with_nul)]
 
+extern crate libc;
 #[macro_use] extern crate rocket;
 
+use std::ffi::{CStr, OsStr, CString, FromVecWithNulError};
 use std::fs::File;
 use std::io::{Error, Read};
+use std::mem::MaybeUninit;
+use std::os::unix::io::FromRawFd;
 use std::process::Command;
 
 use rocket::Request;
 use rocket::response::Responder;
-use std::mem::MaybeUninit;
 
 #[derive(Debug)]
 enum ProcessError {
@@ -35,43 +39,110 @@ impl From<std::io::Error> for ProcessError {
     }
 }
 
+fn c_result(ret: libc::c_int) -> Result<libc::c_int, std::io::Error> {
+    if ret < 0 {
+        Err(std::io::Error::last_os_error())
+    } else {
+        Ok(ret)
+    }
+}
+
+fn make_c_string<S>(s: S) -> Result<CString, FromVecWithNulError>
+where
+    S: AsRef<OsStr>
+{
+    let mut buf = Vec::new();
+    let bytes = s.as_ref().to_str().unwrap().as_bytes();
+    buf.resize(bytes.len(), 0);
+    buf.clone_from_slice(bytes);
+    buf.push(0);
+
+    CString::from_vec_with_nul(buf)
+}
+
+
+fn run_command<I, P, A>(prog: P, args: I) -> Result<String, std::io::Error>
+where
+    P: AsRef<OsStr>,
+    A: AsRef<OsStr>,
+    I: IntoIterator<Item=A>
+{
+    unsafe {
+        let mut p: [libc::c_int; 2] = MaybeUninit::uninit().assume_init();
+        c_result(libc::pipe(p.as_mut_ptr()))?;
+        let pid = c_result(libc::fork())?;
+
+        if pid == 0 {
+            // in child
+
+            // close read end
+            libc::close(p[0]);
+            c_result(libc::dup2(p[1], libc::STDOUT_FILENO))?;
+            c_result(libc::dup2(p[1], libc::STDERR_FILENO))?;
+
+            let prog = make_c_string(prog).unwrap();
+            let args = args.into_iter()
+                .map(|arg| make_c_string(arg).unwrap())
+                .collect::<Vec<_>>();
+
+            let a = std::iter::once(prog.as_ptr())
+                .chain(args.iter().map(|arg| arg.as_ptr()))
+                .chain(std::iter::once(std::ptr::null()))
+                .collect::<Vec<_>>();
+
+            c_result(libc::execv(prog.as_ptr(), a.as_ptr()))?;
+            unreachable!();
+        } else {
+            // in parent
+
+            // close write end
+            libc::close(p[1]);
+            c_result(libc::waitpid(pid, std::ptr::null_mut(), 0))?;
+
+            let mut buf = String::new();
+            File::from_raw_fd(p[0])
+                .read_to_string(&mut buf)?;
+
+            Ok(buf)
+        }
+    }
+}
+
 
 fn command_output(mut cmd: Command) -> Result<String, ProcessError> {
     let status = cmd.status()?;
-    let output = cmd.output()?;
+    let output = String::from_utf8(cmd.output()?.stdout).unwrap();
 
-    let all_out = format!("{}\n{}",
-        String::from_utf8(output.stdout).unwrap(),
-        String::from_utf8(output.stderr).unwrap());
-
-
-    if !status.success() {
-        Err(ProcessError::OtherError(all_out))
+    if status.success() {
+        Ok(output)
     } else {
-        Ok(all_out)
+        Err(ProcessError::OtherError(output))
     }
 }
 
 
 fn get_systemd_unit_status(unit: &str) -> Result<String, ProcessError> {
-    let mut cmd = Command::new("systemctl");
+    let mut cmd = Command::new("sh");
 
-    cmd.arg("status")
-        .arg(unit);
+    cmd.arg("-c")
+        .arg(format!("\"systemctl status '{}' 2>&1\"", unit));
 
     command_output(cmd)
 }
 
 fn get_systemd_status() -> Result<String, ProcessError> {
-    command_output(Command::new("systemctl"))
+    let mut cmd = Command::new("sh");
+    cmd.arg("-c")
+        .arg("\"systemctl 2>&1\"");
+
+    command_output(cmd)
 }
 
 fn get_systemd_unit_journal(unit: &str, since: Option<&str>) -> Result<String, ProcessError> {
-    let mut cmd = Command::new("journalctl");
+    let mut cmd = Command::new("sh");
 
-    cmd.arg("--no-pager")
-        .arg("--unit")
-        .arg(unit);
+    cmd.arg("-c")
+        .arg(format!("\"journalctl --no-pager --unit '{}' 2>&1\"", unit));
 
     if since.is_some() {
         cmd.args(&["--since", since.unwrap()]);
@@ -79,9 +150,6 @@ fn get_systemd_unit_journal(unit: &str, since: Option<&str>) -> Result<String, P
 
     command_output(cmd)
 }
-
-
-
 
 
 #[get("/summary")]
